6c4adcaf9005b016ea0cbd4ee1442d76cea4b11c
Thenjo <36435202+Thenjo@users.noreply.github.com>
2019-01-23 14:41:47 +0530

    Update myflaskapp.py

--- myflaskapp.py
+++ myflaskapp.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template,jsonify
 import json
 import random
 app=Flask(__name__)
-@app.route('/api/quote')
+@app.route("/api/quote")
+def main():
+    return render_template("index.html", value=display())
 def display():
     with open('quotes.json') as json_file:
         data = json.load(json_file)
         res=random.randint(0,len(data))
-        d = render_template('index.html')
-    return (data[res]["quote"],d)
+    return(data[res]["quote"])
+
 if __name__=='__main__':
     app.run(host='localhost', port=8008)
